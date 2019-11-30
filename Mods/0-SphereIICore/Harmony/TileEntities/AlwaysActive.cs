48ad247ae1e61117d2d188d267a796c0e506a276
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-11-30 14:23:14 -0400

    always triggered

--- Mods/0-SphereIICore/Harmony/TileEntities/AlwaysActive.cs
+++ Mods/0-SphereIICore/Harmony/TileEntities/AlwaysActive.cs
@@ -1,65 +1,108 @@
-﻿//using Harmony;
-//using System;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+/*
+ *    <!-- Allows the  Trigger to work -->
+      <property name="AlwaysActive" value="true" />
 
-//public class SphereII_TileEntityAlwaysActive
-//{
+      <!-- How far out the tile entity will re-scan to detect the player -->
+      <property name="ActivationDistance" value="5" />
 
-//    [HarmonyPatch(typeof(TileEntity))]
-//    [HarmonyPatch("IsActive")]
-//    public class SphereII_TileEntity_IsActive
-//    {
-//        public static bool Prefix(ref bool __result, TileEntity __instance, World world)
-//        {
-//            Debug.Log("IsActive?");
-//            BlockValue block = GameManager.Instance.World.GetBlock(__instance.ToWorldPos());
-//            Block block2 = Block.list[block.type];
-//            bool isAlwaysActive = false;
-//            if(block2.Properties.Values.ContainsKey("AlwaysActive"))
-//            {
-//                Debug.Log("Block has AlwaysActive ");
-//                isAlwaysActive = StringParsers.ParseBool(block2.Properties.Values["AlwaysActive"], 0, -1, true);
-//                if(isAlwaysActive)
-//                {
-//                    Debug.Log("Always Active");
-//                    if(block2.RadiusEffects != null)
-//                    {
-//                        Debug.Log("Raidus effects detected");
-//                        List<global::Entity> entitiesInBounds = GameManager.Instance.World.GetEntitiesInBounds(null, new Bounds(__instance.ToWorldPos().ToVector3(), Vector3.one * 5f));
-//                        Debug.Log("Entities in Bound: " + entitiesInBounds.Count);
-//                        if(entitiesInBounds.Count > 0)
-//                        {
-//                            for(int i = 0; i < entitiesInBounds.Count; i++)
-//                            {
-//                                EntityPlayer player = entitiesInBounds[i] as EntityPlayer;
-//                                if(player)
-//                                {
-//                                    Debug.Log("Player in bounds");
-//                                    float distanceSq = entitiesInBounds[i].GetDistanceSq(__instance.ToWorldPos().ToVector3());
-//                                    for(int l = 0; l < block2.RadiusEffects.Length; l++)
-//                                    {
-//                                        BlockRadiusEffect blockRadiusEffect = block2.RadiusEffects[l];
-//                                        Debug.Log("Checking radius: " + blockRadiusEffect.variable);
-//                                        if(distanceSq <= blockRadiusEffect.radius * blockRadiusEffect.radius && player.Buffs.HasBuff(blockRadiusEffect.variable))
-//                                        {
-//                                            block.meta = 0;
-//                                            world.SetBlockRPC(__instance.ToWorldPos(), block);
-//                                            Debug.Log("Activating Block");
-//                                            return true;
-//                                            Block.list[block.type].ActivateBlock(world, __instance.GetClrIdx(), __instance.ToWorldPos(), block, true, true);
-//                                        }
-//                                    }
-//                                }
+          <property name="ActivateOnLook" value="true" />
 
-//                            }
-//                        }
-//                    }
-//                }
+      <!-- Triggers the block if the buff buffCurseSamara is active on the player, or if the player has a cvar called "cvar" with a value of 4, or if myOtherCvar is available, regardless of value -->
+      <property name="ActivationBuffs" value="buffCurseSamara,cvar(4),myOtherCvar" />
+*/
+public class SphereII_TileEntityAlwaysActive
+{
+    [HarmonyPatch(typeof(TileEntity))]
+    [HarmonyPatch("IsActive")]
+    public class SphereII_TileEntity_IsActive
+    {
+        public static bool Prefix(ref bool __result, TileEntity __instance, World world)
+        {
+            BlockValue block = GameManager.Instance.World.GetBlock(__instance.ToWorldPos());
+            Block block2 = Block.list[block.type];
+            bool isAlwaysActive = false;
+            if (block2.Properties.Values.ContainsKey("AlwaysActive"))
+            {
+                isAlwaysActive = StringParsers.ParseBool(block2.Properties.Values["AlwaysActive"], 0, -1, true);
+                if (isAlwaysActive)
+                {
+                    bool blCanTrigger = false;
 
-//            }
+                    // Scan for the player in the radius as defined by the Activation distance of the block
+                    List<Entity> entitiesInBounds = GameManager.Instance.World.GetEntitiesInBounds(null, new Bounds(__instance.ToWorldPos().ToVector3(), Vector3.one * ( block2.GetActivationDistanceSq() / block2.GetActivationDistanceSq() )));
+                    if (entitiesInBounds.Count > 0)
+                    {
+                        for (int i = 0; i < entitiesInBounds.Count; i++)
+                        {
+                            EntityPlayer player = entitiesInBounds[i] as EntityPlayer;
+                            if (player)
+                            {
+                                if (block2.Properties.Values.ContainsKey("ActivationBuffs"))
+                                {
+                                    foreach (String strbuff in block2.Properties.Values["ActivationBuffs"].Split(','))
+                                    {
+                                        String strBuffName = strbuff;
+                                        float CheckValue = -1f;
 
-//            return true;
-//        }
-//    }
-//}
+                                        // Check if there's a ( ) at the end of the buff; this will be used as a cvar hook.
+                                        int start = strbuff.IndexOf('(');
+                                        int end = strbuff.IndexOf(')');
+                                        if (start != -1 && end != -1 && end > start + 1)
+                                        {
+                                            CheckValue = StringParsers.ParseFloat(strbuff.Substring(start + 1, end - start - 1), 0, -1, NumberStyles.Any);
+                                            strBuffName = strbuff.Substring(0, start);
+                                        }
+
+                                        // If the player has a buff by this name, trigger it.
+                                        if (player.Buffs.HasBuff(strBuffName))
+                                            blCanTrigger = true;
+
+                                        // If the player has a cvar, then do some extra checks
+                                        if (player.Buffs.HasCustomVar(strBuffName))
+                                        {
+                                            // If there's no cvar value specified, just allow it.
+                                            if (CheckValue == -1)
+                                                blCanTrigger = true;
+
+                                            // If a cvar is set, then check to see if it matches
+                                            if (CheckValue > -1)
+                                            {
+                                                if (player.Buffs.GetCustomVar(strBuffName) == CheckValue)
+                                                    blCanTrigger = true;
+                                            }
+
+                                            // If any of these conditions match, trigger the Activate block
+                                            if (blCanTrigger)
+                                                break;
+                                        }
+
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+
+                    if (blCanTrigger)
+                    {
+                        Block.list[block.type].ActivateBlock(world, __instance.GetClrIdx(), __instance.ToWorldPos(), block, true, true);
+                        return true;
+                    }
+                    else
+                    {
+                            Block.list[block.type].ActivateBlock(world, __instance.GetClrIdx(), __instance.ToWorldPos(), block, false, true);
+                    }
+                    
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Mods/0-SphereIICore/Scripts/Blocks/BlockTriggeredSDX.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+/*
+     <property name="Class" value="TriggeredSDX, Mods"/>
+     <property name="AlwaysActive" value="true" />
+
+      <!-- How far out the tile entity will re-scan to detect the player -->
+      <property name="ActivationDistance" value="5" />
+
+       <!-- this triggers a SetTrigger("On") when looked at -->
+      <property name="ActivateOnLook" value="true" />
+      
+      <!-- Triggers the block if the buff buffCurseSamara is active on the player, or if the player has a cvar called "cvar" with a value of 4, or if myOtherCvar is available, regardless of value -->
+       <property name="ActivationBuffs" value="buffCurseSamara,cvar(4),myOtherCvar" />
+
+*/
+class BlockTriggeredSDX : BlockLoot
+{
+
+    bool TriggerOnly = false;
+    public override string GetActivationText(WorldBase _world, BlockValue _blockValue, int _clrIdx, Vector3i _blockPos, EntityAlive _entityFocusing)
+    {
+        if (_blockValue.Block.Properties.Values.ContainsKey("ActivateOnLook"))
+        {
+            bool ActivateOnLook = StringParsers.ParseBool(_blockValue.Block.Properties.Values["ActivateOnLook"], 0, -1, true);
+            if (ActivateOnLook)
+            {
+                TriggerOnly = true;
+                ActivateBlock(_world, _clrIdx, _blockPos, _blockValue, true, true);
+                TriggerOnly = false;
+            }
+
+        }
+        return "";
+    }
+
+
+    public override bool ActivateBlock(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, bool isOn, bool isPowered)
+    {
+        // If there's no transform, no sense on keeping going for this class.
+        BlockEntityData _ebcd = _world.GetChunkFromWorldPos(_blockPos).GetBlockEntity(_blockPos);
+        if (_ebcd == null || _ebcd.transform == null)
+            return false;
+
+        Animator[] componentsInChildren = _ebcd.transform.GetComponentsInChildren<Animator>();
+        if (componentsInChildren != null)
+        {
+            for (int i = componentsInChildren.Length - 1; i >= 0; i--)
+            {
+                Animator animator = componentsInChildren[i];
+                if (isOn )
+                {
+                    //Debug.Log("Triggering Animation");
+                    animator.enabled = true;
+                    if ( !TriggerOnly)
+                        animator.SetBool("On", true);
+                    animator.SetTrigger("On");
+                }
+
+                if (isOn == false)
+                {
+                   // Debug.Log("Turning off animator");
+                    animator.SetBool("On", false);
+                    animator.enabled = false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
