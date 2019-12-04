a4627126c755b74139e9705de339eb80190e8ac5
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-12-03 21:40:58 -0400

    Resyncing Winter Project Work

---
+++ Mods/0-SphereIICore/Scripts/Dialogs/DialogActionAddCVar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+public class DialogActionAddCVar : DialogActionAddBuff
+{
+    public string strOperator = "set";
+
+    public override void PerformAction(EntityPlayer player)
+    {
+        if (string.IsNullOrEmpty(Value))
+            Value = "1";
+
+        int flValue = 1;
+        int.TryParse(Value, out flValue);
+
+        
+        if (player.Buffs.HasCustomVar(ID))
+        {
+            float CurrentValue = player.Buffs.GetCustomVar(ID);
+            if (strOperator.ToLower() == "add")
+                CurrentValue += flValue;
+
+            if (strOperator.ToLower() == "sub")
+                CurrentValue -= flValue;
+
+            if (strOperator.ToLower() == "set")
+                CurrentValue = flValue;
+
+            player.Buffs.SetCustomVar(ID, CurrentValue, false);
+            return;
+
+
+        }
+    }
+
+
+}
