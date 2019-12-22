a0e25f3ece591fbfbc171f0a21e239040212e257
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-12-22 11:13:31 -0400

    adding santa dialog

--- Mods/0-SphereIICore/Harmony/ZombieFeatures/InertEntity.cs
+++ Mods/0-SphereIICore/Harmony/ZombieFeatures/InertEntity.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class SphereII_InertEntity
 {
-
+    // <property name="EntityActiveWhen" value="night" />
     public static bool IsInert(EntityAlive alive)
     {
         if (alive == null)
