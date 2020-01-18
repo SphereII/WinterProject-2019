e162b5847fc21b0579a9f614735f3b533ea5135c
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2020-01-18 13:18:37 -0400

    Fixing RWG and Navezgane

--- Mods/SphereII Winter Project/Harmony/PrefabInstance.cs
+++ Mods/SphereII Winter Project/Harmony/PrefabInstance.cs
@@ -29,7 +29,7 @@ public class SphereII_WinterProject
         }
     }
 
-    
+
 
     // Navezgane only - Since it's pregenerated, it uses a different prefabs loading, with preset locations. This will adjust the prefabs for only navezgane.
     public class SphereII_WinterProject_PrefabInstance
@@ -37,21 +37,7 @@ public class SphereII_WinterProject
         public static bool PrefabInstance_Prefix(ref PrefabInstance __instance, ref Prefab _bad)
         {
 
-//            Debug.Log("PrefabInstance");
-//            //  // Only apply these changes to navezgane world
-////            if (GamePrefs.GetString(EnumGamePrefs.GameWorld) == "Navezgane")
-//            {
-//                if (_bad != null)
-//                {
-//                    Debug.Log("Changing Boundary");
-//                    __instance.boundingBoxPosition.y -= 8;
-
-//                    _bad.yOffset = _bad.size.y < 9 ? -(8 - _bad.size.y) : -8;
-//                    _bad.bTraderArea = false;
-//                    _bad.bExcludeDistantPOIMesh = true;
-//                    _bad.bCopyAirBlocks = true;
-//                }
-//            }
+            // No longer needed, but left as an example
             return true;
         }
     }
@@ -62,9 +48,9 @@ public class SphereII_WinterProject
     {
         public static bool Postfix(bool __result, ref Prefab __instance)
         {
-            if(__result)
+            if (__result)
             {
-                if(__instance.size.y < 10)
+                if (__instance.size.y < 10)
                 {
                     Debug.Log("\n**************");
                     Debug.Log("Winter Project Prefab Filter : " + __instance.filename + " yOffset: " + __instance.yOffset + " Size: " + __instance.size.ToString());
@@ -82,32 +68,19 @@ public class SphereII_WinterProject
     }
 
     [HarmonyPatch(typeof(Prefab))]
-    [HarmonyPatch("LoadXMLData")]
-    [HarmonyPatch(new Type[] { typeof(string), typeof(string) })]
-    public class SphereII_WinterProject_LoadXMLData
-    {
-        public static void Postfix(ref Prefab __instance)
-        {
-            //Debug.Log("Prefab: " + __instance.filename + " YOffset: " + __instance.yOffset + " After: " + (__instance.yOffset - -8));
-            //// Prefabs are -1 seems to behave very strangely. Sink them by 8, regardless. Either o one will notice it being one-less snow buried, but may
-            //// result in floating buildings. 
-            //__instance.yOffset -= 8;
-            
-            //__instance.bTraderArea = false;
-            //__instance.bExcludeDistantPOIMesh = true;
-            //__instance.bCopyAirBlocks = true;
-        }
-    }
-
-    [HarmonyPatch(typeof(Prefab))]
-    [HarmonyPatch("CopyIntoLocal")]
+    [HarmonyPatch("CopyBlocksIntoChunkNoEntities")]
     public class SphereII_WinterProject_Prefab_Prefix
     {
-        public static bool Prefix(Prefab __instance, ref Vector3i _destinationPos, ChunkCluster _cluster, QuestTags _questTags)
+        public static bool Prefix(ref Prefab __instance, ref Vector3i _prefabTargetPos)
         {
-            // Debug.Log("CopyIntoLocal(): " + __instance.PrefabName);
-            //_destinationPos.y -= 8;
+            // If they are pre-generated Winter Project worlds, don't apply this. They'd have already been applied.
+            if (GamePrefs.GetString(EnumGamePrefs.GameWorld).ToLower().Contains("winter project"))
+                return true;
 
+            _prefabTargetPos.y -= 8;
+            __instance.bTraderArea = false;
+            __instance.bExcludeDistantPOIMesh = true;
+            __instance.bCopyAirBlocks = true;
             return true;
 
         }
@@ -119,23 +92,37 @@ public class SphereII_WinterProject
     {
         public static void Postfix(Prefab __instance, Vector3i _destinationPos, ChunkCluster _cluster, QuestTags _questTags)
         {
-      //    Debug.Log("CopyIntoLocal(): " + __instance.PrefabName);
-          WinterModPrefab.SetSnowPrefab(__instance, _cluster, _destinationPos, _questTags);
+            WinterModPrefab.SetSnowPrefab(__instance, _cluster, _destinationPos, _questTags);
         }
 
     }
 
- 
+    [HarmonyPatch(typeof(Prefab))]
+    [HarmonyPatch("LoadXMLData")]
+    [HarmonyPatch(new Type[] { typeof(string), typeof(string) })]
+    public class SphereII_WinterProject_Prefab_LoadXMLData
+    {
+        public static void Postfix(bool __result, ref Prefab __instance)
+        {
+            if (__result)
+            {
+                // Distant POI Y Offset: this is what makes the prefabs look like they are on top of the snow, but then drop down.
+                __instance.distantPOIYOffset -= 8;
+                __instance.bExcludeDistantPOIMesh = true;
+            }
+        }
+
+    }
+
     [HarmonyPatch(typeof(PrefabInstance))]
     [HarmonyPatch("CopyIntoChunk")]
     public class SphereII_WinterProject_PrefabInstance_CopyIntoChunk
     {
         public static void Postfix(PrefabInstance __instance, Chunk _chunk)
         {
-         //   Debug.Log("CopyIntoChunk(): " + __instance.prefab.PrefabName);
             WinterModPrefab.SetSnowChunk(_chunk, __instance.boundingBoxPosition, __instance.boundingBoxSize);
         }
     }
 
- 
+
 }
